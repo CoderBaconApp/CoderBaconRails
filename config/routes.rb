135bbf82770ee0f7e892d6d0135de71ad9de818e
Justin Steffen <JustinDSN@gmail.com>
2014-01-22 17:55:40 -0800

    Adding messages controller and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 CoderBaconRails::Application.routes.draw do
   devise_for :users
   root 'home#index'
+  resources :messages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
