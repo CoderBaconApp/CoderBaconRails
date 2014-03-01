40da7021a86dd462a23134e7b6f5aecc26b549e3
Justin Steffen <JustinDSN@gmail.com>
2014-03-01 15:59:33 -0800

    Adding responsive

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,21 @@ class MessagesController < ApplicationController
   def index
     @sent_messages = current_user.sent_messages
     @received_messages = current_user.received_messages
+
+    # @sent_conversations = current_user.sent_messages.select("MAX(id) AS id").select("receiver_id").group(:receiver_id).collect { |x| [x.id, x.receiver_id] }
+    # @received_conversations = current_user.received_messages.select("MAX(id) AS id").select("sender_id").group(:sender_id).collect { |x| [x.id, x.sender_id] }
+
+    # @sent_conversations.zip(@received_conversations).collect do  |arr|
+    #   sent = arr[0]
+    #   received = arr[1]
+
+    #   if sent[0] > received[0]
+    #     return sent[0]
+    #   else
+    #     return received[0]
+    #   end
+    # end
+
   end
 
   def new
@@ -14,8 +29,8 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     @message.sender = current_user
-    # @message.receiver = User.find(:receiver)
     @message.save!
+    render index
   end
 
   private
