5b76f4558df5c75340e7bbe3cfdeed76002a361e
Justin Steffen <JustinDSN@gmail.com>
2014-03-03 11:46:13 -0800

    Adding gravatar_url user helper.  Using gravatar_url for user's image

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,7 @@
+module UsersHelper
+  def avatar_url(user)
+    size = 300
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
+end
