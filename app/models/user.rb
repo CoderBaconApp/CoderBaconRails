110bf149dc0550680be2295342b5b459c6a49fa6
Justin Steffen <JustinDSN@gmail.com>
2014-03-18 19:45:13 -0700

    Adding Username login capability to app.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,20 @@ class User < ActiveRecord::Base
   has_many :sent_messages, :class_name => 'Message', :foreign_key => 'sender_id'
   has_many :listeners
   has_many :conversations, through: :listeners
+
+  validates :username,
+    :uniqueness => {
+      :case_sensitive => false
+    }
+
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
 end
